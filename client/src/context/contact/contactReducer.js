f7e4305797ef6c24050b199010934ec19de26f78
Radu <avadanii@tiscali.it>
2019-07-29 16:04:06 +0300

    contacts api integration

--- client/src/context/contact/contactReducer.js
+++ client/src/context/contact/contactReducer.js
@@ -1,34 +1,54 @@
 import {
+  GET_CONTACTS,
   ADD_CONTACT,
   DELETE_CONTACT,
   SET_CURRENT,
   CLEAR_CURRENT,
+  CONTACT_ERROR,
   UPDATE_CONTACT,
   FILTER_CONTACTS,
-  CLEAR_FILTER
+  CLEAR_FILTER,
+  CLEAR_CONTACTS
 } from '../types';
 
 export default (state, action) => {
   switch (action.type) {
+    case GET_CONTACTS:
+      return {
+        ...state,
+        contacts: action.payload,
+        loading: false
+      };
     case ADD_CONTACT:
       return {
         ...state,
-        contacts: [...state.contacts, action.payload]
+        contacts: [action.payload, ...state.contacts],
+        loading: false
       };
     case DELETE_CONTACT:
       return {
         ...state,
         contacts: state.contacts.filter(
-          contact => contact.id !== action.payload
-        )
+          contact => contact._id !== action.payload
+        ),
+        loading: false
+      };
+    case CLEAR_CONTACTS:
+      return {
+        ...state,
+        contacts: null,
+        filtered: null,
+        error: null,
+        current: null
       };
     case UPDATE_CONTACT:
       return {
         ...state,
         contacts: state.contacts.map(contact => {
-          if (contact.id === action.payload.id) return action.payload;
+          if (contact._id === action.payload._id) return action.payload;
           return contact;
-        })
+        }),
+        loading: false
       };
     case SET_CURRENT:
       return {
@@ -52,6 +72,11 @@ export default (state, action) => {
       return {
         ...state,
         filtered: null
+      };
+    case CONTACT_ERROR:
+      return {
+        ...state,
+        error: action.payload
       };
     default:
       return state;
